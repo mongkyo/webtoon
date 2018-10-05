ad46604ed9e85047ee00345a3b0a5e06a19cce75
JungMongKyo <dreamong91@gmail.com>
2018-10-05 17:05:02 +0900

    Webtoon project

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,34 +1,29 @@
-from bs4 import BeautifulSoup
-import requests
-import re
 import os
-from data import Webtoon, Episode, WebtoonNotExist
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+from data import Episode, Webtoon, WebtoonNotExist
 
 
 class Crawler:
     def __init__(self):
         self._webtoon_dict = {}
-        pass
 
     def get_html(self):
         """
-        전체 웹툰 목록의 HTML을 리턴한다
+        전체 웹툰 목록의 HTML을 리턴한다.
         만약에 파일로 저장되어 있다면, 해당 내용을 읽어온다.
+
         파일로 저장되어 있지 않다면, requests를 사용해 웹에서 받아와 리턴해준다.
             파일위치는 ./saved_data/weekday.html를 사용
             경로 작성에는 os.path모듈을 사용
 
         -> 다 작성한 후에는 show_webtoon_list에서 이 메서드를 사용하도록 함
-        :return:
+        :return: HTML데이터 문자열
         """
-        # 실제 HTTP요청을 매번 할 필요가 없음
-        # -> 파일로 저장해두고 필요할 때 갱신하는 기능이 필요
-        # 1. html파일이 존재하는지 검사
-        #   ./saved_data/weekday.html
-        # 2. 존재한다면 -> 해당파일을 읽어서 html변수에 넣기
-        # 3. 없으면 -> requests로 요청하고 결과 text를 html변수에 넣기
-        # weekday.html 파일의 경로
-
+        # weekday.html파일의 경로
         root = os.path.dirname(os.path.abspath(__file__))
         dir_path = os.path.join(root, 'saved_data')
         file_path = os.path.join(dir_path, 'weekday.html')
@@ -69,7 +64,6 @@ class Crawler:
                 if title not in self._webtoon_dict:
                     new_webtoon = Webtoon(webtoon_id, title, url_thumbnail)
                     self._webtoon_dict[title] = new_webtoon
-
         return self._webtoon_dict
 
     def get_webtoon(self, title):
@@ -84,24 +78,9 @@ class Crawler:
         except KeyError:
             raise WebtoonNotExist(title)
 
-
     def show_webtoon_list(self):
-        """
-        전체 웹툰 제목을 출력해줌
-
-        1. requests를 사용해서 웹툰 목록 URL의 내용을 가져옴
-        2. Beautifulsoup을 사용해서 가져온 HTML데이터를 파싱
-        3. 파싱한 결과를 사용해서 Webtoon클래스 인스턴스들을 생성
-        4. 생성한 인스턴스 목록을 dict에 제목 key를 사용해서 할당
-        5. dict를 순회하며 제목들을 출력
-
-        crawler = Crawler()
-        crawler.show_webtoon_list()
-        :return:
-        """
         for title, webtoon in self.webtoon_dict.items():
             print(title)
-
 
 
 if __name__ == '__main__':
--- crawler/data/webtoon.py
+++ crawler/data/webtoon.py
@@ -11,6 +11,7 @@ __all__ = (
     'WebtoonNotExist',
 )
 
+
 class Webtoon:
     TEST_WEBTOON_ID = 714834
 
@@ -27,56 +28,88 @@ class Webtoon:
     def url(self):
         return f'https://comic.naver.com/webtoon/list.nhn?titleId={self.webtoon_id}'
 
-
     @property
     def episode_dict(self):
         """
+        에피소드 목록을 파싱하는부분을 먼저 연습
+            -> notebook에서
+
         자신의 _episode_dict가 있다면 그대로 리턴
         아니라면 채워준다
-            주소는 (에피소드 리스트 URL)
-            https://comic.naver.com/webtoon/list.nhn?titleId={self.webtoon_id}
+         주소는 (에피소드 리스트 URL)
+          https://comic.naver.com/webtoon/list.nhn?titleId={self.webtoon_id}
 
         키는 Episode의 episode_id
-            Episode URL에서의 'no' Get parameter값에 해당
-            https://comic.naver.com/webtoon/detail.nhn?titleId=651673&no=345&weekday-wed
+            에피소드 상세 URL에서의 'no' GET parameter값에 해당
+            https://comic.naver.com/webtoon/detail.nhn?titleId=651673&no=345&weekday=wed
+
         webtoon_dict를 채울때와 비슷하게, 새로운 Episode클래스 인스턴스를 만들어 할당
 
-        page값을 1부터 늘려가면서 '다음'버튼이 안보일때까지 내용을 가져옴
+        1. 현재는 1페이지 내용만 불러오므로
+           page값을 1부터 늘려가면서 '다음'버튼이 안보일때까지 내용을 가져옴 (페이지 수가 적은 웹툰으로 테스트!)
+        2. pickle을 사용해서 Crawler가 가진 webtoon_dict를 저장, 불러오기 하는 방식으로 중복 데이터를 웹에서 받지 않도록 함
+        3. CLI를 구성해서 사용자가 셸에서 선택해서 웹툰 크롤러 기능을 사용할 수 있도록 만들기 (main.py)
+        4. Episode의 Detail페이지에서 그림을 다운로드 받기
+            requests로 그림 요청시 Referer설정을 해줘야 함 <- 아니면 403또는 400에러 발생
+                headers = {'Referer': 'http://comic.naver.com/webtoon/list.nhn?titleId=<WebtoonId>'}
+                requests.get(<URL>, headers=headers)
+
+            저장시
+                response = requests.get(<URL>)
+                open(<path>, 'wb').write(response.content)
+            코드를 사용
+        5. 다운로드 받은 그림을 볼 수 있는 HTML생성하기
+
         :return:
         """
         if not self._episode_dict:
+            # 비어있는 경우
+           # self.url (목록페이지 URL)에 HTTP요청 후, 받은 결과를 파싱 시작
             response = requests.get(self.url)
             soup = BeautifulSoup(response.text, 'lxml')
 
+            # 에피소드 목록이 table.viewList의 각 'tr'요소 하나씩에 해당함
             table = soup.select_one('table.viewList')
             tr_list = table.select('tr')[1:]
             for tr in tr_list:
+                # 이 루프 내부의 'tr'하나당 에피소드 하나를 만들어야 함
                 try:
+                    # 데이터 파싱
                     td_list = tr.select('td')
                     href = td_list[0].select_one('a')['href']
-
                     no = re.search(r'no=(\d+)', href).group(1)
-                    if no not in self._episode_dict:
-                        url_thumbnail = td_list[0].select_one('img')['src']
-                        title = td_list[1].select_one('a').get_text(strip=True)
-                        rating = td_list[2].select_one('strong').get_text()
-                        created_date = td_list[3].get_text(strip=True)
-
-                        episode = Episode(
-                            episode_id=no,
-                            title=title,
-                            url_thumbnail=url_thumbnail,
-                            rating=rating,
-                            created_date=created_date,
-                        )
-                        self._episode_dict[no] = episode
+                    url_thumbnail = td_list[0].select_one('img')['src']
+                    title = td_list[1].select_one('a').get_text(strip=True)
+                    rating = td_list[2].select_one('strong').get_text()
+                    created_date = td_list[3].get_text(strip=True)
+                    # 파싱한 데이터로 새 Episode인스턴스 생성
+                    episode = Episode(
+                        episode_id=no,
+                        title=title,
+                        url_thumbnail=url_thumbnail,
+                        rating=rating,
+                        created_date=created_date,
+                    )
+                    # 인스턴스의 _episode_dict사전에 episode_id (파싱데이터에서는 'no'변수)키로 인스턴스 할당
+                    self._episode_dict[no] = episode
                 except:
+                    # 위 파싱에 실패하는 경우에는 무시 (tr이 의도와 다르게 생겼을때 실패함)
+                    # 기왕이면 실패 로그를 쌓으면 좋음 (어떤 웹툰의 몇 번째 페이지 몇 번째 row시도중 실패했다를 텍스트 파일에)
                     pass
         return self._episode_dict
+
+    def get_episode(self, index):
+        """
+        index번에 해당하는 에피소드를 자신의 episode_dict프로퍼티를 사용해서 리턴
+        :param index:
+        :return:
+        """
+        pass
+
 
 class WebtoonNotExist(Exception):
     def __init__(self, title):
         self.title = title
 
     def __str__(self):
-        return f'Webtoon(title : {self.title})을 찾을 수 없습니다.'
+        return f'Webtoon(title: {self.title})을 찾을 수 없습니다'
